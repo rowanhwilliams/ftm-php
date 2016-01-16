7fde84bfc01202b5fa0e5bc65a1e42afa1a9f83f
Rowan Williams <rowanhwilliams@hotmail.com>
2016-01-16 21:38:42 +0000

    Added Is_Published to companies resultset

--- app/Http/Controllers/Admin/CompaniesController.php
+++ app/Http/Controllers/Admin/CompaniesController.php
@@ -61,7 +61,7 @@ class CompaniesController extends Controller
 
         $companies = DB::table('Company')
             ->select('Company.id_Company','Company.Company_Full_Name','Company.Year_Founded','Company.Website','Company.id_Employee_Size',
-                     'Employee_Size.Employee_Size', 'Addresses.City', 'Addresses.State', 'Country.Country','Revenue_Stage.Revenue_Stage')
+                     'Employee_Size.Employee_Size', 'Addresses.City', 'Addresses.State', 'Country.Country','Revenue_Stage.Revenue_Stage', 'Company.Is_Published')
             ->leftJoin('Employee_Size', 'Company.id_Employee_Size', '=', 'Employee_Size.id_Employee_Size')
             ->leftJoin('Headquarters_Information', 'Company.id_Company', '=', 'Headquarters_Information.id_Company')
             ->leftJoin('Addresses', 'Headquarters_Information.AddressId', '=', 'Addresses.AddressId')
