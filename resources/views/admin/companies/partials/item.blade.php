308c7724a5d579b17f5e956d6c348422bf563ae5
Rowan Williams <rowanhwilliams@hotmail.com>
2016-02-15 22:38:35 +0000

    trial

--- resources/views/admin/companies/partials/item.blade.php
+++ resources/views/admin/companies/partials/item.blade.php
@@ -140,7 +140,7 @@
 <div class="col-md-6">
     @if ($company->Acquired_Subsidiary == 1)
         <div class="form-group">
-            Subsidiary of {!! $ultimateParentCompany->Company_Full_Name !!}
+            Subsidiary of {!! $ultimateParentCompany !!}
         </div>
     @endif
     <div style="font-size: 18px">Media Contact Information</div>
